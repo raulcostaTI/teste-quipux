e744ed66f47c6bbaca3d83c1555bd199e8357014
Raul Costa <raulcostapro@gmail.com>
2026-08-27 00:44:54 -0300

    feat: autenticacao basica/Spring Security/usuario fixo

--- src/main/java/com/quipux/playlistapi/config/SecurityConfig.java
+++ src/main/java/com/quipux/playlistapi/config/SecurityConfig.java
@@ -2,23 +2,49 @@ package com.quipux.playlistapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    // criptografa as senhas
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // cria um usuario FIXO pra autenticar na API
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
+        UserDetails user = User.builder()
+                .username("admin")
+                .password(encoder.encode("123456"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll() // RETIRAR NÃO ESQUECER APENAS TESTAR O CRUD
-                        .requestMatchers("/lists/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .httpBasic(Customizer.withDefaults()) // Teste de autentição
                 .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
 
         return http.build();
     }
 }
+
+
+// erro 403 na requisição...
