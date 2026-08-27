47538da1a884ee88324b77e7fdee23aeb3482caa
Raul Costa <raulcostapro@gmail.com>
2026-08-26 21:53:08 -0300

    feat: musica e playlist com relacionamento ,lombok e jakarta

---
+++ src/main/java/com/quipux/playlistapi/model/Musica.java
@@ -0,0 +1,28 @@
+package com.quipux.playlistapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString(exclude = "playlist")
+public class Musica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String artista;
+    private String album;
+    private String ano;
+    private String genero;
+
+    // Musica - parte 2 do relacionamento de tabelas.
+    // Este é o lado "dono" do relacionamento: guarda a chave estrangeira playlist_id.
+    @ManyToOne
+    private Playlist playlist;
+}
---
+++ src/main/java/com/quipux/playlistapi/model/Playlist.java
@@ -0,0 +1,29 @@
+package com.quipux.playlistapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString(exclude = "musicas")
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    // Playlist - parte 1 do relacionamento de tabelas.
+    // mappedBy indica quem controla a key é a Musica.
+    @OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Musica> musicas = new ArrayList<>();
+}
